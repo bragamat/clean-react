d58bfd301db5a9eb4aa19e961c80f27e82a1509d
bragamat <mateuscbraga@gmail.com>
2020-11-28 21:37:49 -0300

    test: required field validation

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,21 @@
 import { RequiredFieldError } from "validation/errors/required-field-error";
 import { RequiredFieldValidation } from "./required-field-validation";
 
+import * as Faker from "faker";
+
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation(Faker.database.column());
+
 describe("RequiredFieldValidation", () => {
   it("returns error if field is empty", () => {
-    const sut = new RequiredFieldValidation("email");
-    const error = sut.validate("some_email");
+    const sut = makeSut();
+    const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
+  });
+
+  it("returns falsy if field is not empty", () => {
+    const sut = makeSut();
+    const error = sut.validate(Faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
